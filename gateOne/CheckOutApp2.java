4a863c7acace1b022b65fcb411ca0bcc2e5bc4dc
JohDan <chinedunomeh2015@gmail.com>
2024-08-07 14:27:17 +0100

    commit

--- gateOne/CheckOutApp2.java
+++ gateOne/CheckOutApp2.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;
 
 public class CheckOutApp2{
 	public static void main(String[]args){
