3cc5c5aef5d96c81cdd734a8c656c64534cf0113
JohDan <chinedunomeh2015@gmail.com>
2024-08-07 03:33:57 +0100

    Assignment commit

--- gateOne/CheckOutApp2.java
+++ gateOne/CheckOutApp2.java
@@ -8,9 +8,11 @@ public class CheckOutApp2{
 	Scanner input = new Scanner(System.in);
 		System.out.print("Welcome to SemiColon Super Stores");
 	
+		//ArrayList<String> customersName = new ArrayList<String>();
 		ArrayList<String> productBought = new ArrayList<String>();
 		ArrayList<Integer> numOfUnits = new ArrayList<Integer>();
 		ArrayList<Double> howMuchPerUnits = new ArrayList<Double>();
+		ArrayList<Double> pricePerProduct = new ArrayList<Double>();
 
 		System.out.print("\n\nMAIN BRANCH");
 		System.out.println("\nLOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS");
@@ -19,90 +21,89 @@ public class CheckOutApp2{
 		String pass = "";
 
 		System.out.println("What is the customer's name?");
+		String customersName = input.next();
 		
 		double total = 0.0;
+		double totalBeforeDiscount = 0;
 		double discount =0;
 		double discountableAmount = 0;
 		double subTotal = 0;
 		final double VAT = 7.5 / 100;
 
-		System.out.println("What did the user buy? ");
-			productBought.add(input.next());
 
-			System.out.println("How many pieces?");
-			int pieces = input.nextInt();
-			numOfUnits.add(pieces);
-
-			System.out.println("How much per unit?");
-			double price = input.nextDouble();
-			howMuchPerUnits.add(price);
-			System.out.print(howMuchPerUnits);
-			
-			System.out.println("Add more Items?");
-			pass = input.next();
-
-			System.out.println(price);
-		while(!pass.equalsIgnoreCase("no")){
+	
+		while(!(pass.equalsIgnoreCase("no"))){
 
 			System.out.println("What did the user buy? ");
 			productBought.add(input.next());
-			
 
 			System.out.println("How many pieces?");
-			pieces = input.nextInt();
-			numOfUnits.add(pieces);
+			numOfUnits.add(input.nextInt());
 
 			System.out.println("How much per unit?");
-			price = input.nextDouble();
-			howMuchPerUnits.add(price);
-			System.out.println(price);
+			howMuchPerUnits.add(input.nextDouble());
 			
 			System.out.println("Add more Items?");
 			pass = input.next();
 			
 		}
+		for(int index = 0; index < productBought.size(); index++){	
+		double amount = 0;
+			total = numOfUnits.get(index) * howMuchPerUnits.get(index);
+			pricePerProduct.add(total);
+		}
 
-			//System.out.print(howMuchPerUnits);
-			
-			for(int index = 0; index < productBought.size(); index++){	
-			double amount = 0;
-				total = numOfUnits.get(index) * howMuchPerUnits.get(index);
-				//howMuchPerUnits.add(amount);
+		System.out.print("\nWhat is your name?");
+		String cashiersName = input.next();
+
+		System.out.println("How much discount will he get?");
+		int discountRate = input.nextInt();
+		double rate = (double)discountRate / 100;
+		
+		System.out.print(howMuchPerUnits);
+		System.out.print(pricePerProduct);
+		for(int index=0; index < productBought.size(); index++){
+			totalBeforeDiscount += pricePerProduct.get(index);
+		}
+		discountableAmount = rate * totalBeforeDiscount;
+		double vat = (17.50/100) * totalBeforeDiscount;		
+		discount = (totalBeforeDiscount - discountableAmount) + vat;
+
+		System.out.println("\nSEMICOLON STORES");
+		System.out.println("\nMAIN BRANCH");
+		System.out.println("\nLOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.");
+		System.out.println("\nTEL: 03293828343");
+		System.out.println("\nDATE : 18-AUGUST-24  8:48:11pm");
+		System.out.println("\nCashier: "+cashiersName);
+		System.out.println("\nCustomer Name: "+customersName);
+		System.out.println("\n=======================================================\n");
+		System.out.printf("%20s%10s%10s%15s", "ITEM", "QTY", "PRICE", "TOTAL(NGN)");
+		System.out.print("\n-------------------------------------------------------\n");
+		for(int index = 0; index < productBought.size(); index++){
+			for(int inner = 0; inner < index; inner++){
+				System.out.println("\t"+inner);
 			}
-
-
-
-			System.out.print("\nWhat is your name?");
-			String cashiersName = input.next();
-
-			System.out.println("How much discount will he get?");
-			int discountRate = input.nextInt();
-				
-			//for(int index = 0; index < numOfUnits.size(); index++){
-			//	total += howMuchPerUnits.get(index); 
-			//}
-
-			System.out.print(total);
-
-			discountableAmount = total - ((discountRate * 0.01) * total);
-			System.out.println(discountableAmount);
-
-			//discount = total - discountableAmount;
-			System.out.println("Discount: "+discountableAmount);
-			System.out.print(total);
-
-		
-			
-
-			
-
-			
-			
-			
-
-			
-		
-
+		}
+		for(int index = 0; index < numOfUnits.size(); index++){
+			for(int inner = 0; inner < index; inner++){
+				System.out.println("\t"+inner);
+			}
+		}
+		for(int index = 0; index < howMuchPerUnits.size(); index++){
+			for(int inner = 0; inner < index; inner++){
+				System.out.println("\t"+inner);
+			}
+		}
+		System.out.print("\n-------------------------------------------------------\n");
+		System.out.print("\nSub Total: "+totalBeforeDiscount);	
+		System.out.print("\nDiscount:		 "+discountableAmount);
+		System.out.println("\nVAT @ 17.50%:		"+vat);
+		System.out.println("\n=======================================================\n");	
+		System.out.print("\nBill Total:			 "+discount);
+		System.out.println("\n=======================================================\n");
+		System.out.println("\nTHIS IS NOT A RECIEPT KINDLY PAY "+discount);
+		System.out.println("\n=======================================================\n");
+	
  
 
 
